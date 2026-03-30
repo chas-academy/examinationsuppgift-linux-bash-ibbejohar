a313ca1772bf16ced012019eba6eac9fd0b7afb4
Ibrahim Johar <ibbe.johar@gmail.com>
2026-03-31 00:39:00 +0200

    done

--- create_users.sh
+++ create_users.sh
@@ -1,10 +1,53 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+# Detta kollar root
 check_root(){
-    if [[ ! $USER == "root" ]]; then
+    if [[ ! $(id -un) == "root" ]]; then
         echo "ERROR: not root"
         exit 1
     fi
 }
 
+# Skapar skeletstruktur för undermappar
+create_dir(){
+    mkdir -p skel/Documents
+    mkdir -p skel/Downloads
+    mkdir -p skel/Work
+}
+
+# Rensar skeletstruktur för undermappar
+cleanup(){
+ rm -rf skel
+}
+
+# Skapar användare med hem och undermappar
+# Ägaren bestäms också
+create_user(){
+    for user in "$@"
+    do
+        useradd -m "$user" -k skel
+    done
+}
+
+# Skapar välkommen fil med användares name och befintliga anvädare
+create_welcome_file(){
+    for user in "$@"
+    do
+        # Välkommen meddelande
+        sudo -u "$user" sh -c 'echo "Välkommen $USER" > "$HOME/welcome.txt"'
+        # Filtera endast riktiga användare och lägg till i welcome filen
+        sudo -u "$user" sh -c "awk -F: '\$3 >= 1000 && \$3 <= 9999 {print \$1}' /etc/passwd >> \$HOME/welcome.txt"
+        chmod -R 700 "/home/$user"
+    done
+}
+
+main(){
 check_root
+create_dir
+create_user "$@"
+create_welcome_file "$@"
+cleanup
+}
+
+
+main "$@"
